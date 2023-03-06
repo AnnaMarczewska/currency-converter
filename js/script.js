797ec056103481409adc64bf9b4c6e81b93b1feb
Anna Marczewska <aniqanet@gmail.com>
2023-03-06 14:57:35 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let formElement = document.querySelector (".js-form");
+let currencyElement = document.querySelector (".js-currency");
+let amountElement = document.querySelector (".js-amount");
+let resultElement = document.querySelector (".js-result");
+
+
+let EURrate = 4.74;
+let CHFrate = 4.81;
+let GBRrate = 5.34;
+let USDrate = 4.44;
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let amount = amountElement.value;
+    let currency = currencyElement.value;
+    let result = resultElement.value;
+
+    switch (currency) {
+        case "EUR":
+            result = amount / EURrate;
+            break;
+        case "CHF":
+            result = amount / CHFrate;
+            break;
+        case "GBR":
+            result = amount / GBRrate;
+            break;
+        case "USD":
+            result = amount / USDrate;
+            break;
+                
+    }
+    resultElement.innerText = `${result.toFixed(2)} ${currency}`
+});
